529bf29d8c573c28cde6bda921f275be0afd87c4
noble <noblebarch@gmail.com>
2024-01-11 18:41:18 +0300

    q2

--- helperfunctions2.c
+++ helperfunctions2.c
@@ -9,13 +9,13 @@
  */
 void print_environment(void)
 {
-    char **env = environ;
+char **env = environ;
 
-    while (*env != NULL)
-    {
-        printf("%s\n", *env);
-        env++;
-    }
+while (*env != NULL)
+{
+printf("%s\n", *env);
+env++;
+}
 }
 
 /**
@@ -26,39 +26,37 @@ void print_environment(void)
  */
 int check_command_existence(char *command)
 {
-    char *path;
-    int command_found = 0;
-    char *path_copy;
-    char exec_path[PATH_MAX_LEN];
-    char *path_var = getenv("PATH");
-    if (path_var == NULL)
-    {
-        fprintf(stderr, "Error: PATH environment not found\n");
-        return (0);
-    }
-    path_copy = strdup(path_var);
-    if (path_copy == NULL)
-    {
-        fprintf(stderr, "Error: Memory allocation failed\n");
-        return (0);
-    }
+char *path;
+int command_found = 0;
+char *path_copy;
+char exec_path[PATH_MAX_LEN];
+char *path_var = getenv("PATH");
+	if (path_var == NULL)
+	{
+	fprintf(stderr, "Error: PATH environment not found\n");
+	return (0);
+	}
+	path_copy = strdup(path_var);
+	if (path_copy == NULL)
+	{
+	fprintf(stderr, "Error: Memory allocation failed\n");
+	return (0);
+	}
+	path = strtok(path_copy, ":");
+	while (path != NULL)
+	{
+	snprintf(exec_path, sizeof(exec_path), "%s/%s", path, command);
 
-    path = strtok(path_copy, ":");
-
-    while (path != NULL)
-    {
-        snprintf(exec_path, sizeof(exec_path), "%s/%s", path, command);
-
-        if (access(exec_path, X_OK) == 0)
-        {
-            printf("Executable found at: %s\n", exec_path);
-            command_found = 1;
-            break;
-        }
-        path = strtok(NULL, ":");
-    }
-    free(path_copy);
-    return (command_found);
+	if (access(exec_path, X_OK) == 0)
+	{
+	printf("Executable found at: %s\n", exec_path);
+	command_found = 1;
+	break;
+	}
+	path = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (command_found);
 }
 
 /**
@@ -71,42 +69,38 @@ int check_command_existence(char *command)
  */
 ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
 {
-    size_t bufsize = 0;
-    int c;
-    char *new_ptr;
-    ssize_t len = 0;
+size_t bufsize = 0;
+int c;
+char *new_ptr;
+ssize_t len = 0;
+	if (lineptr == NULL || n == NULL)
+	return (-1);
+	if (*lineptr == NULL)
+	{
+	bufsize = 128;
+	*lineptr = (char *)malloc(bufsize * sizeof(char));
+	if (*lineptr == NULL)
+	return (-1);
+	*n = bufsize;
+	}
+	while ((c = fgetc(stream)) != EOF && c != '\n')
+	{
+	if ((size_t)(len + 1) >= *n)
+	{
+	bufsize *= 2;
+	new_ptr = (char *)realloc(*lineptr, bufsize);
+	if (new_ptr == NULL)
+	return (-1);
+	*lineptr = new_ptr;
+	*n = bufsize;
+	}
+	(*lineptr)[len++] = c;
+	}
+	if (c == EOF && len == 0)
+	return (len);
 
-    if (lineptr == NULL || n == NULL)
-        return (-1);
-
-    if (*lineptr == NULL)
-    {
-        bufsize = 128;
-        *lineptr = (char *)malloc(bufsize * sizeof(char));
-        if (*lineptr == NULL)
-            return (-1);
-        *n = bufsize;
-    }
-
-    while ((c = fgetc(stream)) != EOF && c != '\n')
-    {
-        if ((size_t)(len + 1) >= *n)
-        {
-            bufsize *= 2;
-            new_ptr = (char *)realloc(*lineptr, bufsize);
-            if (new_ptr == NULL)
-                return (-1);
-            *lineptr = new_ptr;
-            *n = bufsize;
-        }
-        (*lineptr)[len++] = c;
-    }
-
-    if (c == EOF && len == 0)
-        return (len);
-
-    (*lineptr)[len] = '\0';
-    return (len);
+	(*lineptr)[len] = '\0';
+	return (len);
 }
 
 /**
@@ -115,24 +109,24 @@ ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
  */
 void change_directory(const char *new_directory)
 {
-    char current_directory[BUFFER_SIZE];
-    if (getcwd(current_directory, sizeof(current_directory)) == NULL)
-    {
-        perror("getcwd() error");
-    }
-    else
-    {
-        if (new_directory == NULL || strlen(new_directory) == 0)
-        {
-            new_directory = getenv("HOME");
-        }
-        if (chdir(new_directory) == 0)
-        {
-            setenv("PWD", new_directory, 1);
-        }
-        else
-        {
-            perror("chdir() error");
-        }
-    }
+char current_directory[BUFFER_SIZE];
+	if (getcwd(current_directory, sizeof(current_directory)) == NULL)
+	{
+	perror("getcwd() error");
+	}
+	else
+	{
+	if (new_directory == NULL || strlen(new_directory) == 0)
+	{
+	new_directory = getenv("HOME");
+	}
+	if (chdir(new_directory) == 0)
+	{
+	setenv("PWD", new_directory, 1);
+	}
+	else
+	{
+	perror("chdir() error");
+	}
+	}
 }
