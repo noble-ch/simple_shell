960e70c0d465e78dccbf2834dcdc9b9fa5088047
noble <noblebarch@gmail.com>
2024-01-11 18:30:48 +0300

    q2

--- helperfunctions3.c
+++ helperfunctions3.c
@@ -4,8 +4,10 @@
 #include <string.h>
 #include <unistd.h>
 
+int last_command_exit_status = 0;
+
 /**
- * *replace_str - Replace all occurrences of a substring in a string
+ * replace str - Replace all occurrences of a substring in a string
  * @str: The original string
  * @find: The substring to find and replace
  * @replace: The replacement substring
@@ -14,40 +16,44 @@
  */
 char *replace_str(char *str, const char *find, const char *replace)
 {
-	char *result;
-	char *ins;
-	char *tmp;
-	size_t len_find;
-	size_t count;
-	size_t len;
+    char *result;
+    char *ins;
+    char *tmp;
+    size_t len_find;
+    size_t len;
+    size_t count;
 
-	len_find = strlen(find);
-	ins = str;
-	count = 0;
-	while ((tmp = strstr(ins, find)) != NULL)
-	{
-		ins = tmp + len_find;
-		count++;
-	}
-	result = (char *)malloc(strlen(str) + (strlen(replace) - len_find)
-			* count + 1);
-	if (result == NULL)
-	{
-		fprintf(stderr, "Memory allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-	result = '\0';
-	ins = str;
-	while (count--)
-	{
-		tmp = strstr(ins, find);
-		len = tmp - ins;
-		strncat(result, ins, len);
-		strcat(result, replace);
-		ins = tmp + len_find;
-	}
-	strcat(result, ins);
-	return (result);
+    len_find = strlen(find);
+    ins = str;
+
+    count = 0;
+    while ((tmp = strstr(ins, find)) != NULL)
+    {
+        ins = tmp + len_find;
+        count++;
+    }
+
+    result = (char *)malloc(strlen(str) + (strlen(replace) - len_find) * count + 1);
+
+    if (result == NULL)
+    {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    *result = '\0';
+
+    ins = str;
+    while (count--)
+    {
+        tmp = strstr(ins, find);
+        len = tmp - ins;
+        strncat(result, ins, len);
+        strcat(result, replace);
+        ins = tmp + len_find;
+    }
+    strcat(result, ins);
+    return (result);
 }
 
 /**
@@ -58,66 +64,66 @@ char *replace_str(char *str, const char *find, const char *replace)
  */
 char *replace_variables(char *command)
 {
-	char *result;
-	char *pos;
-	int last_command_exit_status;
+    char *result = strdup(command);
+    char *pos = strstr(result, "$?");
+    if (pos != NULL)
+    {
+        char status_str[10];
+        snprintf(status_str, sizeof(status_str), "%d", last_command_exit_status);
+        free(result);
+        result = replace_str(command, "$?", status_str);
+    }
 
-	last_command_exit_status = 0;
-	result = strdup(command);
-	pos = strstr(result, "$?");
-	if (pos != NULL)
-	{
-		char status_str[10];
+    pos = strstr(result, "$$");
+    if (pos != NULL)
+    {
+        char pid_str[10];
+        snprintf(pid_str, sizeof(pid_str), "%d", getpid());
+        free(result);
+        result = replace_str(command, "$$", pid_str);
+    }
 
-		snprintf(status_str, sizeof(status_str), "%d", last_command_exit_status);
-		free(result);
-		result = replace_str(command, "$?", status_str);
-	}
-	pos = strstr(result, "$$");
-	if (pos != NULL)
-	{
-		char pid_str[10];
-
-		snprintf(pid_str, sizeof(pid_str), "%d", getpid());
-		free(result);
-		result = replace_str(command, "$$", pid_str);
-	}
-	return (result);
+    return result;
 }
 
 /**
  * handle_commands - Handle multiple commands separated by logical operators
  * @commands: The input commands string
- *
- * Return: void
  */
 void handle_commands(char *commands)
 {
-	char *token;
-	/* char *logical_op; */
-	/* int status; */
-	int last_command_exit_status;
+    char *token = strtok(commands, ";");
+    char *processed_token;
+    char *logical_op;
+    char *args[MAX_ARGS];
 
-	last_command_exit_status = 0;
-	token = commands;
-	while (*commands != '\0')
-	{
-		if (*commands == ';')
-		{
-			*commands = '\0';
-			token = commands + 1;
-		}
-		commands++;
-	}
-	while (token != NULL)
-	{
-		if (token[0] == '#')
-			break;
-		if (token[strlen(token) - 1] == '\n')
-			token[strlen(token) - 1] = '\0';
-		process_token(token, &last_command_exit_status);
-		token = commands;
-	}
+    while (token != NULL)
+    {
+        if (token[0] == '#')
+            break;
+        if (token[strlen(token) - 1] == '\n')
+            token[strlen(token) - 1] = '\0';
+
+        processed_token = replace_variables(token);
+        parse_command(processed_token, args);
+        handle_command(args);
+        free(processed_token);
+
+        logical_op = strstr(token, "&&");
+        if (logical_op != NULL)
+        {
+            if (last_command_exit_status != 0)
+                break;
+        }
+        else
+        {
+            logical_op = strstr(token, "||");
+            if (logical_op != NULL && last_command_exit_status == 0)
+                break;
+        }
+
+        token = strtok(NULL, ";");
+    }
 }
 
 /**
@@ -128,62 +134,91 @@ void handle_commands(char *commands)
  */
 int handle_command(char **args)
 {
-	int status;
+    int status = 0;
 
-	status = 0;
-	if (strcmp(args[0], "exit") == 0)
-	{
-		handle_exit(args, &status);
-	}
-	else if (strcmp(args[0], "env") == 0)
-	{
-		print_environment();
-	}
-	else if (strcmp(args[0], "setenv") == 0)
-	{
-		handle_setenv(args);
-	}
-	else if (strcmp(args[0], "unsetenv") == 0)
-	{
-		handle_unsetenv(args);
-	}
-	else if (strcmp(args[0], "alias") == 0)
-	{
-		handle_alias(args);
-	}
-	else if (strcmp(args[0], "cd") == 0)
-	{
-		change_directory(args[1]);
-	}
-	else
-	{
-		handle_external_command(args, &status);
-	}
-	return (status);
-}
-
-/**
- * handle_exit - Handle the exit command and exit the shell
- * @args: Array of command arguments
- * @status: Pointer to the exit status
- *
- * Description: This function handles the exit command and exits the
- * shell with the specified status. If a status is provided as an argument
- * the shell exits with that status. If no status is provided, the shell
- * exits with EXIT_SUCCESS.
- */
-
-void handle_exit(char **args, int *status)
-{
-	if (args[1] != NULL)
-	{
-		*status = atoi(args[1]);
-		printf("Exiting the shell with status code: %d\n", *status);
-		exit(*status);
-	}
-	else
-	{
-		printf("Exiting the shell\n");
-		exit(EXIT_SUCCESS);
-	}
+    if (strcmp(args[0], "exit") == 0)
+    {
+        if (args[1] != NULL)
+        {
+            status = atoi(args[1]);
+            printf("Exiting the shell with status code: %d\n", status);
+            exit(status);
+        }
+        else
+        {
+            printf("Exiting the shell\n");
+            exit(EXIT_SUCCESS);
+        }
+    }
+    else if (strcmp(args[0], "env") == 0)
+    {
+        print_environment();
+    }
+    else if (strcmp(args[0], "setenv") == 0)
+    {
+        if (args[1] != NULL && args[2] != NULL)
+        {
+            if (setenv(args[1], args[2], 1) != 0)
+                fprintf(stderr, "Failed to set env variable\n");
+        }
+        else
+        {
+            fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
+        }
+    }
+    else if (strcmp(args[0], "unsetenv") == 0)
+    {
+        if (args[1] != NULL)
+        {
+            if (unsetenv(args[1]) != 0)
+                fprintf(stderr, "Failed to unset env variable\n");
+        }
+        else
+        {
+            fprintf(stderr, "Usage: unsetenv VARIABLE\n");
+        }
+    }
+    else if (strcmp(args[0], "alias") == 0)
+    {
+        if (args[1] == NULL)
+        {
+            print_aliases();
+        }
+        else if (args[2] == NULL)
+        {
+            print_alias(args[1]);
+        }
+        else
+        {
+            int arg_index = 1;
+            while (args[arg_index] != NULL)
+            {
+                char *equal_sign = strchr(args[arg_index], '=');
+                if (equal_sign != NULL)
+                {
+                    *equal_sign = '\0';
+                    define_alias(args[arg_index], equal_sign + 1);
+                }
+                else
+                {
+                    fprintf(stderr, "Invalid alias definition: %s\n", args[arg_index]);
+                }
+                arg_index++;
+            }
+        }
+    }
+    else if (strcmp(args[0], "cd") == 0)
+    {
+        change_directory(args[1]);
+    }
+    else
+    {
+        if (!check_command_existence(args[0]))
+        {
+            printf("Command not found: %s\n", args[0]);
+            status = 1;
+        }
+        execute_command(args);
+    }
+    return status;
 }
--- helperfunctions4.c
+++ helperfunctions4.c
@@ -1,22 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+
+Alias aliases[MAX_ALIASES];
+int alias_count = 0;
 
 /**
  * print_aliases - Prints all defined aliases
  */
-void print_aliases(void)
+void print_aliases()
 {
-	int i;
-	int alias_count;
-	Alias aliases[MAX_ALIASES];
-
-	alias_count = 0;
-	for (i = 0; i < alias_count; i++)
-	{
-		printf("%s='%s'\n", aliases[i].name, aliases[i].value);
-	}
+    int i;
+    for (i = 0; i < alias_count; i++)
+    {
+        printf("%s='%s'\n", aliases[i].name, aliases[i].value);
+    }
 }
 
 /**
@@ -25,18 +23,14 @@ void print_aliases(void)
  */
 void print_alias(const char *name)
 {
-	int i;
-	int alias_count;
-	Alias aliases[MAX_ALIASES];
-
-	alias_count = 0;
-	for (i = 0; i < alias_count; i++)
-	{
-		if (strcmp(aliases[i].name, name) == 0)
-		{
-			printf("%s='%s'\n", aliases[i].name, aliases[i].value);
-		}
-	}
+    int i;
+    for (i = 0; i < alias_count; i++)
+    {
+        if (strcmp(aliases[i].name, name) == 0)
+        {
+            printf("%s='%s'\n", aliases[i].name, aliases[i].value);
+        }
+    }
 }
 
 /**
@@ -46,79 +40,31 @@ void print_alias(const char *name)
  */
 void define_alias(const char *name, const char *value)
 {
-	int existing_alias_index;
-	int i;
-	int alias_count;
-	Alias aliases[MAX_ALIASES];
-
-	existing_alias_index = -1;
-	alias_count = 0;
-	for (i = 0; i < alias_count; i++)
-	{
-		if (strcmp(aliases[i].name, name) == 0)
-		{
-			existing_alias_index = i;
-			break;
-		}
-	}
-	if (existing_alias_index != -1)
-	{
-		strcpy(aliases[existing_alias_index].value, value);
-	}
-	else
-	{
-		if (alias_count < MAX_ALIASES)
-		{
-			strcpy(aliases[alias_count].name, name);
-			strcpy(aliases[alias_count].value, value);
-			alias_count++;
-		}
-		else
-		{
-			fprintf(stderr, "Max number of aliases reached\n");
-		}
-	}
-}
-
-/**
- * process_token - Process the given token
- * @token: The token to be processed
- * @last_command_exit_status: Pointer to the exit status of the last command
- */
-
-void process_token(char *token, int *last_command_exit_status)
-{
-	char *processed_token;
-	char *args[MAX_ARGS];
-	char *logical_op;
-
-	logical_op = strstr(token, "&&");
-	processed_token = replace_variables(token);
-	parse_command(processed_token, args);
-	*last_command_exit_status = handle_command(args);
-	free(processed_token);
-	check_logical_ops(logical_op, last_command_exit_status);
-}
-
-/**
- * check_logical_ops - Check for logical operators in the token
- * @logical_op: Pointer to the logical operator in the token
- * @last_command_exit_status: Pointer to the exit status of the last command
- */
-
-void check_logical_ops(char *logical_op, int *last_command_exit_status)
-{
-	char *token = NULL;
-
-	if (logical_op != NULL)
-	{
-		if (*last_command_exit_status != 0)
-			return;
-	}
-	else
-	{
-		token = strstr(logical_op, "||");
-		if (token != NULL && *last_command_exit_status == 0)
-			return;
-	}
+    int existing_alias_index = -1;
+    int i;
+    for (i = 0; i < alias_count; i++)
+    {
+        if (strcmp(aliases[i].name, name) == 0)
+        {
+            existing_alias_index = i;
+            break;
+        }
+    }
+    if (existing_alias_index != -1)
+    {
+        strcpy(aliases[existing_alias_index].value, value);
+    }
+    else
+    {
+        if (alias_count < MAX_ALIASES)
+        {
+            strcpy(aliases[alias_count].name, name);
+            strcpy(aliases[alias_count].value, value);
+            alias_count++;
+        }
+        else
+        {
+            fprintf(stderr, "Max number of aliases reached\n");
+        }
+    }
 }
